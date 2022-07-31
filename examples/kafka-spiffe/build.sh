844b062de5db9092823ba12af55beb1e7534c4bd
Amit Lichtenberg <amitlicht@gmail.com>
2022-07-31 15:39:33 +0300

    Remove irrelevant kafka-spiffe example (#12)
    Co-authored-by: Amit Lichtenberg <amitlicht@otterize.com>

--- examples/kafka-spiffe/build.sh
+++
@@ -1,5 +0,0 @@
-# /bin/sh
-
-docker buildx build --platform linux/x86_64 -f ./src/spiffe-java-helper/Dockerfile ./src/ --tag kafka-spiffe-sidecar
-docker tag kafka-spiffe-sidecar:latest public.ecr.aws/e3b4k2v5/kafka-spiffe-sidecar:latest
-docker push public.ecr.aws/e3b4k2v5/kafka-spiffe-sidecar:latest
