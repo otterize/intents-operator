11ca38dceb824c78c5b51845f35a97793263ea9b
Ori Shoshan <ori@otterize.com>
2022-08-30 18:05:49 +0300

    Add events reporting for spire-integration-operator

--- src/operator/controllers/pod_controller.go
+++ src/operator/controllers/pod_controller.go
@@ -15,6 +15,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"strconv"
@@ -45,6 +46,7 @@ type PodReconciler struct {
 	SpireClient     spireclient.ServerClient
 	EntriesRegistry entries.Registry
 	SecretsManager  secrets.Manager
+	EventRecorder   record.EventRecorder
 }
 
 // +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;update;patch
@@ -135,7 +137,7 @@ func (r *PodReconciler) updatePodLabel(ctx context.Context, pod *corev1.Pod, lab
 	return ctrl.Result{}, nil
 }
 
-func (r *PodReconciler) generatePodTLSSecret(ctx context.Context, pod *corev1.Pod, serviceName string, entryID string, entryHash string) error {
+func (r *PodReconciler) ensurePodTLSSecret(ctx context.Context, pod *corev1.Pod, serviceName string, entryID string, entryHash string) error {
 	log := logrus.WithFields(logrus.Fields{"pod": pod.Name, "namespace": pod.Namespace})
 	if pod.Annotations == nil || pod.Annotations[TLSSecretNameAnnotation] == "" {
 		log.WithField("annotation.key", TLSSecretNameAnnotation).Info("skipping TLS secrets creation - annotation not found")
@@ -171,8 +173,8 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	log := logrus.WithField("pod", req.NamespacedName)
 
 	// Fetch the Pod from the Kubernetes API.
-	var pod corev1.Pod
-	if err := r.Get(ctx, req.NamespacedName, &pod); err != nil {
+	pod := &corev1.Pod{}
+	if err := r.Get(ctx, req.NamespacedName, pod); err != nil {
 		if apierrors.IsNotFound(err) {
 			// we'll ignore not-found errors, since they can't be fixed by an immediate
 			// requeue (we'll need to wait for a new notification), and we can get them
@@ -186,38 +188,49 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	log.Info("updating SPIRE entries & secrets for pod")
 
 	// resolve pod to otterize service name
-	serviceName, err := r.resolvePodToServiceName(ctx, &pod)
+	serviceName, err := r.resolvePodToServiceName(ctx, pod)
 	if err != nil {
+		r.EventRecorder.Eventf(pod, corev1.EventTypeWarning, "Pod owner resolving failed", "could not resolve pod to its owner: %s", err.Error())
 		return ctrl.Result{}, err
 	}
 
 	// Ensure the ServiceNameSelectorLabel is set
-	result, err := r.updatePodLabel(ctx, &pod, ServiceNameSelectorLabel, serviceName)
-	if err != nil || !result.IsZero() {
+	result, err := r.updatePodLabel(ctx, pod, ServiceNameSelectorLabel, serviceName)
+	if err != nil || result.Requeue {
+		r.EventRecorder.Event(pod, corev1.EventTypeWarning, "Pod label update failed", err.Error())
 		return result, err
 	}
 
 	dnsNames, err := r.resolvePodToCertDNSNames(pod)
 	if err != nil {
-		return ctrl.Result{}, fmt.Errorf("error resolving pod cert DNS names, will continue with an empty DNS names list: %w", err)
+		err = fmt.Errorf("error resolving pod cert DNS names, will continue with an empty DNS names list: %w", err)
+		r.EventRecorder.Event(pod, corev1.EventTypeWarning, "Resolving cert DNS names failed", err.Error())
+		return ctrl.Result{}, err
 	}
 
-	ttl := r.resolvePodToCertTTl(pod)
+	ttl, err := r.resolvePodToCertTTl(pod)
+	if err != nil {
+		r.EventRecorder.Event(pod, corev1.EventTypeWarning, "Getting cert TTL failed", err.Error())
+		return ctrl.Result{}, err
+	}
 
 	// Add spire-server entry for pod
 	entryID, err := r.EntriesRegistry.RegisterK8SPodEntry(ctx, pod.Namespace, ServiceNameSelectorLabel, serviceName, ttl, dnsNames)
 	if err != nil {
 		log.WithError(err).Error("failed registering SPIRE entry for pod")
+		r.EventRecorder.Event(pod, corev1.EventTypeWarning, "Failed registering SPIRE entry", err.Error())
 		return ctrl.Result{}, err
 	}
 
 	hashStr, err := r.getEntryHash(pod.Namespace, serviceName, ttl, dnsNames)
 	if err != nil {
+		r.EventRecorder.Event(pod, corev1.EventTypeWarning, "Failed calculating SPIRE entry hash", err.Error())
 		return ctrl.Result{}, err
 	}
 
 	// generate TLS secret for pod
-	if err := r.generatePodTLSSecret(ctx, &pod, serviceName, entryID, hashStr); err != nil {
+	if err := r.ensurePodTLSSecret(ctx, pod, serviceName, entryID, hashStr); err != nil {
+		r.EventRecorder.Event(pod, corev1.EventTypeWarning, "Failed creating TLS secret for pod", err.Error())
 		return ctrl.Result{}, err
 	}
 
@@ -234,7 +247,7 @@ func (r *PodReconciler) getEntryHash(namespace string, serviceName string, ttl i
 	return hashStr, nil
 }
 
-func (r *PodReconciler) resolvePodToCertDNSNames(pod corev1.Pod) ([]string, error) {
+func (r *PodReconciler) resolvePodToCertDNSNames(pod *corev1.Pod) ([]string, error) {
 	if len(pod.Annotations[DNSNamesAnnotation]) == 0 {
 		return nil, nil
 	}
@@ -248,20 +261,19 @@ func (r *PodReconciler) resolvePodToCertDNSNames(pod corev1.Pod) ([]string, erro
 	return dnsNames, nil
 }
 
-func (r *PodReconciler) resolvePodToCertTTl(pod corev1.Pod) int32 {
+func (r *PodReconciler) resolvePodToCertTTl(pod *corev1.Pod) (int32, error) {
 	ttlString := pod.Annotations[CertTTLAnnotation]
 	if len(ttlString) == 0 {
-		return 0
+		return 0, nil
 	}
 
 	ttl64, err := strconv.ParseInt(ttlString, 0, 32)
 
 	if err != nil {
-		logrus.Warnf("Failed cconverting ttl: %s str to Int. %s", ttlString, err)
-		return 0
+		return 0, fmt.Errorf("failed converting ttl: %s str to int. %w", ttlString, err)
 	}
 
-	return int32(ttl64)
+	return int32(ttl64), nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
