c63f33def157610a6ab6393080cb4cd4f6afce19
Ori Shoshan <ori@otterize.com>
2022-10-17 19:09:43 +0300

    Add LICENSE file, remove examples dir

--- examples/netpol-defaultdeny/http_client/build.sh
+++
@@ -1,8 +0,0 @@
-# /bin/sh
-REPO=353146681200.dkr.ecr.us-east-1.amazonaws.com
-set -ex
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $REPO
-
-IMAGE=$REPO/otterize-tools:http-client-test
-docker buildx build --platform linux/amd64 -f ./client.Dockerfile . -t $IMAGE
-docker push $IMAGE
--- examples/netpol-defaultdeny/http_server/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/otterize/spire-integration-operator/examples/http_server
-
-go 1.18
