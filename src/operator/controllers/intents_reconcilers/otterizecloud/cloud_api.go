a808771b6584a92afea7c6ca3b04403d3cceaf43
Ori Shoshan <ori@otterize.com>
2023-01-15 18:47:50 +0200

    Preparation for Otterize Cloud release (#113)
    Co-authored-by: Evyatar Meged <evyatar.m@otterize.com>
    Co-authored-by: Netanel Bollag <54136032+NetanelBollag@users.noreply.github.com>
    Co-authored-by: Amit Lichtenberg <amitlicht@gmail.com>
    Co-authored-by: Amit Itzkovitch <amit.i@otterize.com>

---
+++ src/operator/controllers/intents_reconcilers/otterizecloud/cloud_api.go
@@ -0,0 +1,82 @@
+package otterizecloud
+
+import (
+	"context"
+	"github.com/Khan/genqlient/graphql"
+	otterizev1alpha1 "github.com/otterize/intents-operator/src/operator/api/v1alpha1"
+	"github.com/otterize/intents-operator/src/shared/otterizecloud/graphqlclient"
+	"github.com/otterize/intents-operator/src/shared/otterizecloud/otterizecloudclient"
+	"github.com/sirupsen/logrus"
+)
+
+type CloudClient interface {
+	ReportKafkaServerConfig(ctx context.Context, server graphqlclient.KafkaServerConfigInput) error
+	ReportAppliedIntents(ctx context.Context, namespace string, clientIntentsList otterizev1alpha1.ClientIntentsList) error
+	ReportIntentsOperatorConfiguration(ctx context.Context, enableEnforcement bool) error
+	ReportComponentStatus(ctx context.Context, component graphqlclient.ComponentType)
+}
+
+type CloudClientImpl struct {
+	client graphql.Client
+}
+
+func NewClient(ctx context.Context) (CloudClient, bool, error) {
+	client, ok, err := otterizecloudclient.NewClient(ctx)
+	if !ok {
+		return nil, false, nil
+	} else if err != nil {
+		return nil, true, err
+	}
+
+	return &CloudClientImpl{client: client}, true, nil
+}
+
+func (c *CloudClientImpl) ReportKafkaServerConfig(ctx context.Context, server graphqlclient.KafkaServerConfigInput) error {
+	_, err := graphqlclient.ReportKafkaServerConfig(ctx, c.client, server)
+	if err != nil {
+		return err
+	}
+
+	logrus.Infof("Successfully reported KafkaServerConfig # %+v # to Otterize cloud", server)
+
+	return nil
+}
+
+func (c *CloudClientImpl) ReportAppliedIntents(
+	ctx context.Context,
+	namespace string,
+	clientIntentsList otterizev1alpha1.ClientIntentsList) error {
+
+	intentsInput, err := clientIntentsList.FormatAsOtterizeIntents()
+	if err != nil {
+		return err
+	}
+
+	_, err = graphqlclient.ReportAppliedKubernetesIntents(ctx, c.client, namespace, intentsInput)
+	if err != nil {
+		return err
+	}
+	logrus.Infof("New intents count for namespace %s: %d", namespace, len(intentsInput))
+	return nil
+}
+
+func (c *CloudClientImpl) ReportIntentsOperatorConfiguration(
+	ctx context.Context,
+	enableEnforcement bool) error {
+	_, err := graphqlclient.ReportIntentsOperatorConfiguration(ctx, c.client, graphqlclient.IntentsOperatorConfigurationInput{
+		EnableEnforcement: enableEnforcement,
+	})
+	if err != nil {
+		return err
+	}
+	logrus.Info("Intents operator configuration reported to cloud successfully")
+	return nil
+}
+
+func (c *CloudClientImpl) ReportComponentStatus(ctx context.Context, component graphqlclient.ComponentType) {
+	_, err := graphqlclient.ReportComponentStatus(ctx, c.client, component)
+	if err != nil {
+		logrus.WithError(err).Error("failed to reported component status Otterize cloud")
+		return
+	}
+}
---
+++ src/shared/otterizecloud/graphqlclient/generate.go
@@ -0,0 +1,3 @@
+package graphqlclient
+
+//go:generate go run github.com/Khan/genqlient ./genqlient.yaml
