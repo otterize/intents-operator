6b74dd843db6f78fd572d348220a68b349830c41
Amit Lichtenberg <amitlicht@otterize.com>
2023-02-21 11:49:48 +0200

    Support custom root CAs when initializing cloud client

--- src/shared/otterizecloud/otterizecloudclient/config.go
+++ src/shared/otterizecloud/otterizecloudclient/config.go
@@ -10,6 +10,7 @@ const (
 	ApiClientSecretKey             = "client-secret"
 	OtterizeAPIAddressKey          = "api-address"
 	CloudClientTimeoutKey          = "cloud-client-timeout"
+	OtterizeAPIExtraCAPEMPathsKey  = "api-extra-ca-pem"
 	CloudClientTimeoutDefault      = "30s"
 	OtterizeAPIAddressDefault      = "https://app.otterize.com/api"
 	ComponentReportIntervalKey     = "component-report-interval"
@@ -21,6 +22,7 @@ func init() {
 	viper.SetDefault(OtterizeAPIAddressKey, OtterizeAPIAddressDefault)
 	viper.SetDefault(ComponentReportIntervalKey, ComponentReportIntervalDefault)
 	viper.SetDefault(CloudClientTimeoutKey, CloudClientTimeoutDefault)
+	viper.SetDefault(OtterizeAPIExtraCAPEMPathsKey, []string{})
 	viper.SetEnvPrefix(EnvPrefix)
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	viper.AutomaticEnv()
