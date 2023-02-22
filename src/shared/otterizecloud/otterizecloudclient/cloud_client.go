cc3058d73a967fa846def1a8362daede66e1d782
Amit Lichtenberg <amitlicht@gmail.com>
2023-02-22 17:32:46 +0200

    Support custom root CAs when initializing cloud client (#133)

--- src/shared/otterizecloud/otterizecloudclient/cloud_client.go
+++ src/shared/otterizecloud/otterizecloudclient/cloud_client.go
@@ -2,13 +2,17 @@ package otterizecloudclient
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"errors"
 	"fmt"
 	"github.com/Khan/genqlient/graphql"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/clientcredentials"
 	"net/http"
+	"os"
 )
 
 func NewClient(ctx context.Context) (graphql.Client, bool, error) {
@@ -16,6 +20,7 @@ func NewClient(ctx context.Context) (graphql.Client, bool, error) {
 	secret := viper.GetString(ApiClientSecretKey)
 	apiAddress := viper.GetString(OtterizeAPIAddressKey)
 	clientTimeout := viper.GetDuration(CloudClientTimeoutKey)
+	extraCAPEMPaths := viper.GetStringSlice(OtterizeAPIExtraCAPEMPathsKey)
 	if clientID == "" && secret == "" {
 		return nil, false, nil
 	}
@@ -33,9 +38,29 @@ func NewClient(ctx context.Context) (graphql.Client, bool, error) {
 		AuthStyle:    oauth2.AuthStyleInParams,
 	}
 
+	rootCAs, err := x509.SystemCertPool()
+	if err != nil {
+		return nil, false, fmt.Errorf("error loading root system cert pool: %w", err)
+	}
+
+	for _, path := range extraCAPEMPaths {
+		logrus.Infof("Loading root CA from cert PEM file at '%s'", path)
+		cert, err := os.ReadFile(path)
+		if err != nil {
+			logrus.Errorf("Error loading cert PEM file at '%s', trying to continue without it: %s", path, err)
+			continue
+		}
+
+		if ok := rootCAs.AppendCertsFromPEM(cert); !ok {
+			logrus.Warnf("Failed appending cert PEM file at '%s', trying to continue without it", path)
+		}
+	}
+
+	transport := &http.Transport{TLSClientConfig: &tls.Config{RootCAs: rootCAs}}
+
 	// Timeout for oauth token acquisition is set by http client passed to the token source context
 	// See example 'Example (CustomHTTP)' in https://pkg.go.dev/golang.org/x/oauth2
-	clientWithTimeout := &http.Client{Timeout: clientTimeout}
+	clientWithTimeout := &http.Client{Timeout: clientTimeout, Transport: transport}
 	ctxWithClient := context.WithValue(ctx, oauth2.HTTPClient, clientWithTimeout)
 
 	tokenSrc := cfg.TokenSource(ctxWithClient)
