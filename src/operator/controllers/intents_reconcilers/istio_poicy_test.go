953195de3a96c45ab3829c49b4556c2feffda0cf
Netanel Bollag <54136032+NetanelBollag@users.noreply.github.com>
2023-03-07 19:45:06 +0200

    Netanelb/create istio reconciler (#137)

---
+++ src/operator/controllers/intents_reconcilers/istio_poicy_test.go
@@ -0,0 +1,177 @@
+package intents_reconcilers
+
+import (
+	"context"
+	otterizev1alpha2 "github.com/otterize/intents-operator/src/operator/api/v1alpha2"
+	"github.com/otterize/intents-operator/src/shared/testbase"
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/record"
+	"path/filepath"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"testing"
+)
+
+type IstioPolicyReconcilerTestSuite struct {
+	testbase.ControllerManagerTestSuiteBase
+	Reconciler *IstioPolicyReconciler
+}
+
+func (s *IstioPolicyReconcilerTestSuite) SetupSuite() {
+	s.TestEnv = &envtest.Environment{}
+	var err error
+	s.TestEnv.CRDDirectoryPaths = []string{filepath.Join("..", "..", "config", "crd")}
+
+	s.RestConfig, err = s.TestEnv.Start()
+	s.Require().NoError(err)
+	s.Require().NotNil(s.RestConfig)
+
+	s.K8sDirectClient, err = kubernetes.NewForConfig(s.RestConfig)
+	s.Require().NoError(err)
+	s.Require().NotNil(s.K8sDirectClient)
+
+	err = otterizev1alpha2.AddToScheme(s.TestEnv.Scheme)
+	s.Require().NoError(err)
+}
+
+func (s *IstioPolicyReconcilerTestSuite) BeforeTest(_, testName string) {
+	s.ControllerManagerTestSuiteBase.BeforeTest("", testName)
+	s.Reconciler = NewIstioPolicyReconciler(s.Mgr.GetClient(), s.TestEnv.Scheme, true, true)
+}
+
+func (s *IstioPolicyReconcilerTestSuite) SetupTest() {
+	s.ControllerManagerTestSuiteBase.SetupTest()
+}
+
+func (s *IstioPolicyReconcilerTestSuite) TestGlobalEnforcementDisabled() {
+	intents, err := s.AddIntents("test-intents", "test-client", []otterizev1alpha2.Intent{{
+		Type: otterizev1alpha2.IntentTypeHTTP, Name: "test-server",
+	},
+	})
+	s.Require().NoError(err)
+	s.Require().True(s.Mgr.GetCache().WaitForCacheSync(context.Background()))
+
+	reconciler := NewIstioPolicyReconciler(s.Mgr.GetClient(), s.TestEnv.Scheme, true, false)
+	recorder := record.NewFakeRecorder(100)
+	reconciler.InjectRecorder(recorder)
+	res, err := reconciler.Reconcile(context.Background(), ctrl.Request{
+		NamespacedName: types.NamespacedName{
+			Namespace: s.TestNamespace,
+			Name:      intents.Name,
+		},
+	})
+
+	s.Require().NoError(err)
+	s.Require().Empty(res)
+
+	select {
+	case event := <-recorder.Events:
+		s.Require().Contains(event, ReasonEnforcementGloballyDisabled)
+	default:
+		s.Fail("event not raised")
+	}
+}
+
+func (s *IstioPolicyReconcilerTestSuite) TestIstioPolicyEnforcementDisabled() {
+	intents, err := s.AddIntents("test-intents", "test-client", []otterizev1alpha2.Intent{{
+		Type: otterizev1alpha2.IntentTypeHTTP, Name: "test-server",
+	},
+	})
+	s.Require().NoError(err)
+	s.Require().True(s.Mgr.GetCache().WaitForCacheSync(context.Background()))
+
+	reconciler := NewIstioPolicyReconciler(s.Mgr.GetClient(), s.TestEnv.Scheme, false, true)
+	recorder := record.NewFakeRecorder(100)
+	reconciler.InjectRecorder(recorder)
+	res, err := reconciler.Reconcile(context.Background(), ctrl.Request{
+		NamespacedName: types.NamespacedName{
+			Namespace: s.TestNamespace,
+			Name:      intents.Name,
+		},
+	})
+
+	s.Require().NoError(err)
+	s.Require().Empty(res)
+
+	select {
+	case event := <-recorder.Events:
+		s.Require().Contains(event, ReasonIstioPolicyCreationDisabled)
+	default:
+		s.Fail("event not raised")
+	}
+}
+
+func (s *IstioPolicyReconcilerTestSuite) TestIstioPolicyFinalizerAddedAndRemove() {
+	intentObjectName := "finalizer-intents"
+	intents, err := s.AddIntents(intentObjectName, "test-client", []otterizev1alpha2.Intent{{
+		Type: otterizev1alpha2.IntentTypeHTTP, Name: "test-server",
+	},
+	})
+	s.Require().NoError(err)
+	s.Require().True(s.Mgr.GetCache().WaitForCacheSync(context.Background()))
+
+	res, err := s.Reconciler.Reconcile(context.Background(), ctrl.Request{
+		NamespacedName: types.NamespacedName{
+			Namespace: s.TestNamespace,
+			Name:      intents.Name,
+		},
+	})
+
+	s.Require().NoError(err)
+	s.Require().Empty(res)
+	s.Require().True(s.Mgr.GetCache().WaitForCacheSync(context.Background()))
+
+	intents = &otterizev1alpha2.ClientIntents{}
+	s.WaitUntilCondition(func(assert *assert.Assertions) {
+		err = s.Mgr.GetClient().Get(context.Background(), types.NamespacedName{
+			Namespace: s.TestNamespace,
+			Name:      intentObjectName,
+		}, intents)
+		assert.NoError(err)
+		assert.NotEmpty(intents.Finalizers)
+	})
+	s.Require().Contains(intents.Finalizers, IstioPolicyFinalizerName)
+
+	additionalFinalizerIntents := intents.DeepCopy()
+	// We have to add another finalizer so the object won't actually be deleted after the reconciler finishes
+	additionalFinalizerIntents.Finalizers = append(additionalFinalizerIntents.Finalizers, "finalizer-to-prevent-obj-deletion")
+	err = s.Mgr.GetClient().Patch(context.Background(), additionalFinalizerIntents, client.MergeFrom(intents))
+	s.Require().NoError(err)
+	s.Require().True(s.Mgr.GetCache().WaitForCacheSync(context.Background()))
+
+	err = s.Mgr.GetClient().Delete(context.Background(), intents, &client.DeleteOptions{GracePeriodSeconds: lo.ToPtr(int64(0))})
+	s.Require().NoError(err)
+	s.Require().True(s.Mgr.GetCache().WaitForCacheSync(context.Background()))
+
+	s.WaitForDeletionToBeMarked(intents)
+
+	res = ctrl.Result{Requeue: true}
+	for res.Requeue {
+		res, err = s.Reconciler.Reconcile(context.Background(), ctrl.Request{
+			NamespacedName: types.NamespacedName{
+				Namespace: s.TestNamespace,
+				Name:      intents.Name,
+			},
+		})
+		s.Require().NoError(err)
+		s.Require().True(s.Mgr.GetCache().WaitForCacheSync(context.Background()))
+	}
+
+	intents = &otterizev1alpha2.ClientIntents{}
+	s.WaitUntilCondition(func(assert *assert.Assertions) {
+		err = s.Mgr.GetCache().Get(context.Background(), types.NamespacedName{
+			Namespace: s.TestNamespace, Name: "finalizer-intents",
+		}, intents)
+		assert.True(len(intents.Finalizers) == 1 && intents.Finalizers[0] != otterizev1alpha2.NetworkPolicyFinalizerName)
+	})
+	s.Require().NotContains(intents.Finalizers, IstioPolicyFinalizerName)
+}
+
+func TestIstioPolicyReconcilerTestSuite(t *testing.T) {
+	suite.Run(t, new(IstioPolicyReconcilerTestSuite))
+}
---
+++ src/operator/controllers/intents_reconcilers/istio_policy.go
@@ -0,0 +1,105 @@
+package intents_reconcilers
+
+import (
+	"context"
+	otterizev1alpha2 "github.com/otterize/intents-operator/src/operator/api/v1alpha2"
+	"github.com/otterize/intents-operator/src/shared/injectablerecorder"
+	"github.com/sirupsen/logrus"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+const (
+	IstioPolicyFinalizerName          = "intents.otterize.com/istio-policy-finalizer"
+	ReasonIstioPolicyCreationDisabled = "IstioPolicyCreationDisabled"
+	ReasonRemovingIstioPolicyFailed   = "RemovingIstioPolicyFailed"
+)
+
+type IstioPolicyReconciler struct {
+	client.Client
+	Scheme                     *runtime.Scheme
+	enableIstioPolicyCreation  bool
+	enforcementEnabledGlobally bool
+	injectablerecorder.InjectableRecorder
+}
+
+func NewIstioPolicyReconciler(
+	c client.Client,
+	s *runtime.Scheme,
+	enableIstioPolicyCreation bool,
+	enforcementEnabledGlobally bool) *IstioPolicyReconciler {
+	return &IstioPolicyReconciler{
+		Client:                     c,
+		Scheme:                     s,
+		enableIstioPolicyCreation:  enableIstioPolicyCreation,
+		enforcementEnabledGlobally: enforcementEnabledGlobally,
+	}
+}
+
+func (r *IstioPolicyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	intents := &otterizev1alpha2.ClientIntents{}
+	err := r.Get(ctx, req.NamespacedName, intents)
+	if err != nil {
+		if k8serrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	if intents.Spec == nil {
+		return ctrl.Result{}, nil
+	}
+
+	logrus.Infof("Reconciling Istio authorization policies for service %s in namespace %s",
+		intents.Spec.Service.Name, req.Namespace)
+
+	if !intents.DeletionTimestamp.IsZero() {
+		err := r.cleanFinalizerAndPolicies(ctx, intents)
+		if err != nil {
+			if k8serrors.IsConflict(err) {
+				return ctrl.Result{Requeue: true}, nil
+			}
+			r.RecordWarningEventf(intents, ReasonRemovingIstioPolicyFailed, "could not remove istio policies: %s", err.Error())
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{}, nil
+	}
+
+	if !controllerutil.ContainsFinalizer(intents, IstioPolicyFinalizerName) {
+		logrus.WithField("namespacedName", req.String()).Infof("Adding finalizer %s", IstioPolicyFinalizerName)
+		controllerutil.AddFinalizer(intents, IstioPolicyFinalizerName)
+		if err := r.Update(ctx, intents); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	if !r.enforcementEnabledGlobally {
+		r.RecordNormalEvent(intents, ReasonEnforcementGloballyDisabled, "Enforcement is disabled globally, istio policy creation skipped")
+		return ctrl.Result{}, nil
+	}
+
+	if !r.enableIstioPolicyCreation {
+		r.RecordNormalEvent(intents, ReasonIstioPolicyCreationDisabled, "Istio policy creation is disabled, creation skipped")
+		return ctrl.Result{}, nil
+	}
+
+	// TODO: handle authorization policies creation
+
+	return ctrl.Result{}, nil
+}
+
+func (r *IstioPolicyReconciler) cleanFinalizerAndPolicies(ctx context.Context, intents *otterizev1alpha2.ClientIntents) error {
+	if !controllerutil.ContainsFinalizer(intents, IstioPolicyFinalizerName) {
+		return nil
+	}
+
+	logrus.Infof("Removing Istio policies for deleted intents for service: %s", intents.Spec.Service.Name)
+
+	// TODO: remove authorization policies
+
+	controllerutil.RemoveFinalizer(intents, IstioPolicyFinalizerName)
+	return r.Update(ctx, intents)
+}
