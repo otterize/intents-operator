11fca24d0af9fa73eee2b096921c4a91a03c7b3f
Netanel Bollag <netanel.b@otterize.com>
2023-03-30 20:11:21 +0300

    Add HTTP enforcement

--- src/shared/istiopolicy/creator_test.go
+++ src/shared/istiopolicy/creator_test.go
@@ -104,6 +104,359 @@ func (s *CreatorTestSuite) TestCreate() {
 	s.expectEvent(ReasonCreatedIstioPolicy)
 }
 
+func (s *CreatorTestSuite) TestCreateHTTPResources() {
+	clientName := "test-client"
+	serverName := "test-server"
+	policyName := "authorization-policy-to-test-server-from-test-client.test-namespace"
+	clientIntentsNamespace := "test-namespace"
+
+	intents := &v1alpha2.ClientIntents{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      policyName,
+			Namespace: clientIntentsNamespace,
+		},
+		Spec: &v1alpha2.IntentsSpec{
+			Service: v1alpha2.Service{
+				Name: clientName,
+			},
+			Calls: []v1alpha2.Intent{
+				{
+					Name: serverName,
+					Type: v1alpha2.IntentTypeHTTP,
+					HTTPResources: []v1alpha2.HTTPResource{
+						{
+							Path: "/login",
+							Methods: []v1alpha2.HTTPMethod{
+								v1alpha2.HTTPMethodGet,
+								v1alpha2.HTTPMethodPost,
+							},
+						},
+						{
+							Path: "/logout",
+							Methods: []v1alpha2.HTTPMethod{
+								v1alpha2.HTTPMethodPost,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	clientServiceAccountName := "test-client-sa"
+
+	principal := generatePrincipal(clientIntentsNamespace, clientServiceAccountName)
+	newPolicy := &v1beta1.AuthorizationPolicy{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      policyName,
+			Namespace: clientIntentsNamespace,
+			Labels: map[string]string{
+				v1alpha2.OtterizeServerLabelKey:           "test-server-test-namespace-8ddecb",
+				v1alpha2.OtterizeIstioClientAnnotationKey: "test-client-test-namespace-537e87",
+			},
+		},
+		Spec: v1beta12.AuthorizationPolicy{
+			Selector: &v1beta13.WorkloadSelector{
+				MatchLabels: map[string]string{
+					v1alpha2.OtterizeServerLabelKey: "test-server-test-namespace-8ddecb",
+				},
+			},
+			Rules: []*v1beta12.Rule{
+				{
+					To: []*v1beta12.Rule_To{
+						{
+							Operation: &v1beta12.Operation{
+								Paths: []string{
+									"/login",
+								},
+								Methods: []string{
+									"GET",
+									"POST",
+								},
+							},
+						},
+						{
+							Operation: &v1beta12.Operation{
+								Paths: []string{
+									"/logout",
+								},
+								Methods: []string{
+									"POST",
+								},
+							},
+						},
+					},
+					From: []*v1beta12.Rule_From{
+						{
+							Source: &v1beta12.Source{
+								Principals: []string{
+									principal,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	s.mockClient.EXPECT().List(gomock.Any(), gomock.Any(), gomock.AssignableToTypeOf(client.MatchingLabels{})).Return(nil)
+	s.mockClient.EXPECT().Create(gomock.Any(), newPolicy).Return(nil)
+
+	err := s.creator.Create(context.Background(), intents, clientIntentsNamespace, clientServiceAccountName)
+	s.NoError(err)
+	s.expectEvent(ReasonCreatedIstioPolicy)
+}
+
+func (s *CreatorTestSuite) TestUpdateHTTPResources() {
+	clientName := "test-client"
+	serverName := "test-server"
+	policyName := "authorization-policy-to-test-server-from-test-client.test-namespace"
+	clientIntentsNamespace := "test-namespace"
+
+	intents := &v1alpha2.ClientIntents{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      policyName,
+			Namespace: clientIntentsNamespace,
+		},
+		Spec: &v1alpha2.IntentsSpec{
+			Service: v1alpha2.Service{
+				Name: clientName,
+			},
+			Calls: []v1alpha2.Intent{
+				{
+					Name: serverName,
+					Type: v1alpha2.IntentTypeHTTP,
+					HTTPResources: []v1alpha2.HTTPResource{
+						{
+							Path: "/login",
+							Methods: []v1alpha2.HTTPMethod{
+								v1alpha2.HTTPMethodGet,
+								v1alpha2.HTTPMethodPost,
+							},
+						},
+						{
+							Path: "/logout",
+							Methods: []v1alpha2.HTTPMethod{
+								v1alpha2.HTTPMethodPost,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	clientServiceAccountName := "test-client-sa"
+
+	principal := generatePrincipal(clientIntentsNamespace, clientServiceAccountName)
+	newPolicy := &v1beta1.AuthorizationPolicy{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      policyName,
+			Namespace: clientIntentsNamespace,
+			Labels: map[string]string{
+				v1alpha2.OtterizeServerLabelKey:           "test-server-test-namespace-8ddecb",
+				v1alpha2.OtterizeIstioClientAnnotationKey: "test-client-test-namespace-537e87",
+			},
+		},
+		Spec: v1beta12.AuthorizationPolicy{
+			Selector: &v1beta13.WorkloadSelector{
+				MatchLabels: map[string]string{
+					v1alpha2.OtterizeServerLabelKey: "test-server-test-namespace-8ddecb",
+				},
+			},
+			Rules: []*v1beta12.Rule{
+				{
+					To: []*v1beta12.Rule_To{
+						{
+							Operation: &v1beta12.Operation{
+								Paths: []string{
+									"/login",
+								},
+								Methods: []string{
+									"GET",
+									"POST",
+								},
+							},
+						},
+						{
+							Operation: &v1beta12.Operation{
+								Paths: []string{
+									"/logout",
+								},
+								Methods: []string{
+									"POST",
+								},
+							},
+						},
+					},
+					From: []*v1beta12.Rule_From{
+						{
+							Source: &v1beta12.Source{
+								Principals: []string{
+									principal,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	existingPolicyWithoutHTTP := &v1beta1.AuthorizationPolicy{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      policyName,
+			Namespace: clientIntentsNamespace,
+			Labels: map[string]string{
+				v1alpha2.OtterizeServerLabelKey:           "test-server-test-namespace-8ddecb",
+				v1alpha2.OtterizeIstioClientAnnotationKey: "test-client-test-namespace-537e87",
+			},
+		},
+		Spec: v1beta12.AuthorizationPolicy{
+			Selector: &v1beta13.WorkloadSelector{
+				MatchLabels: map[string]string{
+					v1alpha2.OtterizeServerLabelKey: "test-server-test-namespace-8ddecb",
+				},
+			},
+			Rules: []*v1beta12.Rule{
+				{
+					From: []*v1beta12.Rule_From{
+						{
+							Source: &v1beta12.Source{
+								Principals: []string{
+									principal,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	s.mockClient.EXPECT().List(gomock.Any(), gomock.Any(), gomock.Any()).Do(
+		func(_ context.Context, policies *v1beta1.AuthorizationPolicyList, _ ...client.ListOption) {
+			policies.Items = append(policies.Items, existingPolicyWithoutHTTP)
+		}).Return(nil)
+
+	s.mockClient.EXPECT().Patch(gomock.Any(), gomock.Any(), gomock.AssignableToTypeOf(client.MergeFrom(existingPolicyWithoutHTTP))).Do(
+		func(_ context.Context, policy *v1beta1.AuthorizationPolicy, patch client.Patch, _ ...client.PatchOption) {
+			s.Equal(newPolicy.Name, policy.Name)
+			s.Equal(newPolicy.Namespace, policy.Namespace)
+			s.Equal(newPolicy.Spec.Selector, policy.Spec.Selector)
+			s.Equal(newPolicy.Spec.Rules, policy.Spec.Rules)
+			s.Equal(newPolicy.Labels, policy.Labels)
+			s.Equal(newPolicy.Spec.Rules[0].To[0].Operation, policy.Spec.Rules[0].To[0].Operation)
+			s.Equal(newPolicy.Spec.Rules[0].To[1].Operation, policy.Spec.Rules[0].To[1].Operation)
+		}).Return(nil)
+
+	err := s.creator.Create(context.Background(), intents, clientIntentsNamespace, clientServiceAccountName)
+	s.NoError(err)
+	s.expectEvent(ReasonCreatedIstioPolicy)
+}
+
+func (s *CreatorTestSuite) TestNothingToUpdateHTTPResources() {
+	clientName := "test-client"
+	serverName := "test-server"
+	policyName := "authorization-policy-to-test-server-from-test-client.test-namespace"
+	clientIntentsNamespace := "test-namespace"
+
+	intents := &v1alpha2.ClientIntents{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      policyName,
+			Namespace: clientIntentsNamespace,
+		},
+		Spec: &v1alpha2.IntentsSpec{
+			Service: v1alpha2.Service{
+				Name: clientName,
+			},
+			Calls: []v1alpha2.Intent{
+				{
+					Name: serverName,
+					Type: v1alpha2.IntentTypeHTTP,
+					HTTPResources: []v1alpha2.HTTPResource{
+						{
+							Path: "/login",
+							Methods: []v1alpha2.HTTPMethod{
+								v1alpha2.HTTPMethodGet,
+								v1alpha2.HTTPMethodPost,
+							},
+						},
+						{
+							Path: "/logout",
+							Methods: []v1alpha2.HTTPMethod{
+								v1alpha2.HTTPMethodPost,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	clientServiceAccountName := "test-client-sa"
+
+	principal := generatePrincipal(clientIntentsNamespace, clientServiceAccountName)
+	existingPolicy := &v1beta1.AuthorizationPolicy{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      policyName,
+			Namespace: clientIntentsNamespace,
+			Labels: map[string]string{
+				v1alpha2.OtterizeServerLabelKey:           "test-server-test-namespace-8ddecb",
+				v1alpha2.OtterizeIstioClientAnnotationKey: "test-client-test-namespace-537e87",
+			},
+		},
+		Spec: v1beta12.AuthorizationPolicy{
+			Selector: &v1beta13.WorkloadSelector{
+				MatchLabels: map[string]string{
+					v1alpha2.OtterizeServerLabelKey: "test-server-test-namespace-8ddecb",
+				},
+			},
+			Rules: []*v1beta12.Rule{
+				{
+					To: []*v1beta12.Rule_To{
+						{
+							Operation: &v1beta12.Operation{
+								Paths: []string{
+									"/login",
+								},
+								Methods: []string{
+									"GET",
+									"POST",
+								},
+							},
+						},
+						{
+							Operation: &v1beta12.Operation{
+								Paths: []string{
+									"/logout",
+								},
+								Methods: []string{
+									"POST",
+								},
+							},
+						},
+					},
+					From: []*v1beta12.Rule_From{
+						{
+							Source: &v1beta12.Source{
+								Principals: []string{
+									principal,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	s.mockClient.EXPECT().List(gomock.Any(), gomock.Any(), gomock.Any()).Do(
+		func(_ context.Context, policies *v1beta1.AuthorizationPolicyList, _ ...client.ListOption) {
+			policies.Items = append(policies.Items, existingPolicy)
+		}).Return(nil)
+
+	err := s.creator.Create(context.Background(), intents, clientIntentsNamespace, clientServiceAccountName)
+	s.NoError(err)
+	s.expectEvent(ReasonCreatedIstioPolicy)
+}
+
 func (s *CreatorTestSuite) TestNamespaceNotAllowed() {
 	ctx := context.Background()
 	clientName := "test-client"
