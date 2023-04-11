261af692bdc08d18760547cf22be538aa6236d9f
Netanel Bollag <netanel.b@otterize.com>
2023-04-11 17:03:38 +0300

    Fix copy crd

--- src/operator/copy-manifests-to-helm.sh
+++ src/operator/copy-manifests-to-helm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-TARGET_DIR=../../helm-charts/intents-operator/templates
+TEMPLATE_DIR=../../helm-charts/intents-operator/templates
+CRD_DIR=../../helm-charts/intents-operator/crds
 
 # rename ctd files
 src_dir=config/crd
@@ -11,10 +12,10 @@ for src_file in $(ls -p $src_dir | grep -v /); do
   src_name=$(echo $src_file | sed -e "s/^$src_prefix//" -e "s/$src_suffix//");
   target_file=$(echo $src_name""$target_suffix)
   src_path=$(echo $src_dir"/"$src_file)
-  target_path=$(echo $TARGET_DIR"/"$target_file)
+  target_path=$(echo $CRD_DIR"/"$target_file)
   cp $src_path $target_path
 done
 
 # copy webhook and cluster role
-cp ./config/webhook/manifests-patched $TARGET_DIR"/"ValidatingWebhookConfiguration.yaml
-cp ./config/rbac/role.yaml $TARGET_DIR"/"intents-operator-manager-clusterrole.yaml
+cp ./config/webhook/manifests-patched $TEMPLATE_DIR"/"ValidatingWebhookConfiguration.yaml
+cp ./config/rbac/role.yaml $TEMPLATE_DIR"/"intents-operator-manager-clusterrole.yaml
