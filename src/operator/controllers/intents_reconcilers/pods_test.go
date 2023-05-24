efe52c699fea558d4e65fb2baef1270709a2d573
Netanel Bollag <netanel.b@otterize.com>
2023-05-24 14:29:19 +0300

    move matcher to file

--- src/operator/controllers/intents_reconcilers/pods_test.go
+++ src/operator/controllers/intents_reconcilers/pods_test.go
@@ -21,37 +21,6 @@ const (
 	testNamespace = "test-namespace"
 )
 
-type ClientPatch struct {
-	client.Patch
-}
-
-func (p ClientPatch) Matches(x interface{}) bool {
-	patch := x.(client.Patch)
-	actualData, err := patch.Data(nil)
-	if err != nil {
-		return false
-	}
-
-	expectedData, err := p.Data(nil)
-	if err != nil {
-		return false
-	}
-
-	return string(actualData) == string(expectedData) && patch.Type() == p.Type()
-}
-
-func (p ClientPatch) String() string {
-	data, err := p.Data(nil)
-	if err != nil {
-		return "format error"
-	}
-	return string(data)
-}
-
-func MatchPatch(patch client.Patch) gomock.Matcher {
-	return ClientPatch{patch}
-}
-
 type PodLabelReconcilerTestSuite struct {
 	suite.Suite
 	Reconciler *PodLabelReconciler
