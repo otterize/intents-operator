c1049339aa01cec86b051b46a761176ef4c59954
Netanel Bollag <netanel.b@otterize.com>
2023-05-28 15:48:02 +0300

    Remove comment

--- src/operator/controllers/intents_reconcilers/generate.go
+++ src/operator/controllers/intents_reconcilers/generate.go
@@ -1,5 +1,4 @@
 package intents_reconcilers
 
 //go:generate go run github.com/golang/mock/mockgen@v1.6.0 -destination=./mocks/mock_k8s_client.go -package=intentsreconcilersmocks sigs.k8s.io/controller-runtime/pkg/client Client
-// Generate mocks for the EndpointsReconciler
 //go:generate go run github.com/golang/mock/mockgen@v1.6.0 -destination=./mocks/mock_endpoints_reconciler.go -package=intentsreconcilersmocks -source=../external_traffic/endpoints_reconciler.go EndpointsReconciler
