794fe804cc804af4ecabfa271835455fbc35a909
Netanel Bollag <netanel.b@otterize.com>
2023-06-12 15:55:49 +0300

    Fix kafka files names

--- src/operator/controllers/kafkaacls/generate.go
+++ src/operator/controllers/kafkaacls/generate.go
@@ -1,4 +1,4 @@
 package kafkaacls
 
 //go:generate go run github.com/golang/mock/mockgen@v1.6.0 -destination=./mocks/mock_cluster_admin.go -package=kafkaaclsmocks github.com/Shopify/sarama ClusterAdmin
-//go:generate go run github.com/golang/mock/mockgen@v1.6.0 -destination=./mocks/mock_intents_admin.go -package=kafkaaclsmocks -source=intentsAdmin.go ClusterAdmin
+//go:generate go run github.com/golang/mock/mockgen@v1.6.0 -destination=./mocks/mock_intents_admin.go -package=kafkaaclsmocks -source=intents_admin.go ClusterAdmin
