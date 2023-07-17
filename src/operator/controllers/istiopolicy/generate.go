da726897d05ffb3bcc559828d6861cd39609aa62
Netanel Bollag <netanel.b@otterize.com>
2023-07-17 10:41:49 +0300

    remove istio CRD

--- src/operator/controllers/istiopolicy/generate.go
+++ src/operator/controllers/istiopolicy/generate.go
@@ -1,4 +1,3 @@
 package istiopolicy
 
-//go:generate curl -sL https://raw.githubusercontent.com/istio/api/master/kubernetes/customresourcedefinitions.gen.yaml -o ./crd/istio-all-crd.yaml
 //go:generate go run github.com/golang/mock/mockgen -package istiopolicymocks -destination ./mocks/mock_client.go sigs.k8s.io/controller-runtime/pkg/client Client
