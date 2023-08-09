05ef890cdef50ba1b0df6f8ccb318fb45c2607b9
Netanel Bollag <netanel.b@otterize.com>
2023-08-09 17:24:46 +0300

    Add test case for server with no policy

--- src/operator/controllers/protected_service_reconcilers/policy_cleaner_test.go
+++ src/operator/controllers/protected_service_reconcilers/policy_cleaner_test.go
@@ -318,6 +318,112 @@ func (s *PolicyCleanerReconcilerTestSuite) TestUnprotectedServerWithAccessPolicy
 	s.Require().NoError(err)
 }
 
+func (s *PolicyCleanerReconcilerTestSuite) TestServerWithoutPolicyNothingShouldHappen() {
+	selector, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{MatchExpressions: []metav1.LabelSelectorRequirement{
+		{
+			Key:      otterizev1alpha2.OtterizeNetworkPolicy,
+			Operator: metav1.LabelSelectorOpExists,
+		},
+		{
+			Key:      otterizev1alpha2.OtterizeNetworkPolicyExternalTraffic,
+			Operator: metav1.LabelSelectorOpDoesNotExist,
+		},
+		{
+			Key:      otterizev1alpha2.OtterizeNetworkPolicyServiceDefaultDeny,
+			Operator: metav1.LabelSelectorOpDoesNotExist,
+		},
+	}})
+	s.Require().NoError(err)
+
+	networkPolicies := []v1.NetworkPolicy{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "policy-name",
+				Namespace: testNamespace,
+				Labels: map[string]string{
+					otterizev1alpha2.OtterizeNetworkPolicy: protectedServiceFormattedName,
+				},
+			},
+			Spec: v1.NetworkPolicySpec{
+				PolicyTypes: []v1.PolicyType{v1.PolicyTypeIngress},
+				PodSelector: metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						otterizev1alpha2.OtterizeServerLabelKey: protectedServiceFormattedName,
+					},
+				},
+				Ingress: []v1.NetworkPolicyIngressRule{
+					{
+						From: []v1.NetworkPolicyPeer{
+							{
+								PodSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										fmt.Sprintf(
+											otterizev1alpha2.OtterizeAccessLabelKey, protectedServiceFormattedName): "true",
+									},
+								},
+								NamespaceSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										otterizev1alpha2.OtterizeNamespaceLabelKey: testNamespace,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	s.Client.EXPECT().List(
+		gomock.Any(),
+		gomock.Eq(&v1.NetworkPolicyList{}),
+		&client.ListOptions{Namespace: testNamespace, LabelSelector: selector},
+	).DoAndReturn(
+		func(ctx context.Context, list *v1.NetworkPolicyList, opts ...client.ListOption) error {
+			list.Items = append(list.Items, networkPolicies...)
+			return nil
+		})
+
+	var protectedServicesResources otterizev1alpha2.ProtectedServiceList
+	protectedServicesResources.Items = []otterizev1alpha2.ProtectedService{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      protectedServicesResourceName,
+				Namespace: testNamespace,
+			},
+			Spec: otterizev1alpha2.ProtectedServiceSpec{
+				Name: protectedService,
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      anotherProtectedServiceResourceName,
+				Namespace: testNamespace,
+			},
+			Spec: otterizev1alpha2.ProtectedServiceSpec{
+				Name: anotherProtectedService,
+			},
+		},
+	}
+
+	s.Client.EXPECT().List(gomock.Any(), gomock.Eq(&otterizev1alpha2.ProtectedServiceList{}), &client.ListOptions{Namespace: testNamespace}).DoAndReturn(
+		func(ctx context.Context, list *otterizev1alpha2.ProtectedServiceList, opts ...client.ListOption) error {
+			protectedServicesResources.DeepCopyInto(list)
+			return nil
+		})
+
+	request := ctrl.Request{
+		NamespacedName: types.NamespacedName{
+			Namespace: testNamespace,
+			Name:      protectedServicesResourceName,
+		},
+	}
+
+	res, err := s.reconciler.Reconcile(context.Background(), request)
+	s.Require().Empty(res)
+	s.Require().NoError(err)
+}
+
 func (s *PolicyCleanerReconcilerTestSuite) TestNoNetworkPolicies() {
 	selector, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{MatchExpressions: []metav1.LabelSelectorRequirement{
 		{
