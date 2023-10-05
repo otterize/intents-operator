7da0e188d9115001e992a131ecae53de7f43091d
omri.s <omri.s@otterize.com>
2023-10-05 14:42:32 +0300

    merge from main

--- src/operator/copy-manifests-to-helm.sh
+++ src/operator/copy-manifests-to-helm.sh
@@ -8,13 +8,18 @@ src_dir=config/crd
 src_prefix=k8s.otterize.com_
 target_suffix=-customresourcedefinition.yaml
 src_suffix=.yaml
-for src_file in $(ls -p $src_dir | grep -v /); do
+for src_file in $(ls -p $src_dir | grep -v / | grep -v kustom | grep -v patched); do
   src_name=$(echo $src_file | sed -e "s/^$src_prefix//" -e "s/$src_suffix//");
   target_file=$(echo $src_name""$target_suffix)
   src_path=$(echo $src_dir"/"$src_file)
   target_path=$(echo $CRD_DIR"/"$target_file)
   cp $src_path $target_path
 done
+# copy clientIntents CRD
+src_name=$(echo k8s.otterize.com_clientintents.yaml | sed -e "s/^$src_prefix//" -e "s/$src_suffix//");
+target_file=$(echo $src_name""$target_suffix);
+target_path=$(echo $CRD_DIR"/"$target_file);
+cp ./config/crd/k8s.otterize.com_clientintents-patched.yaml $target_path
 
 # copy webhook and cluster role
 cp ./config/webhook/manifests-patched $TEMPLATE_DIR"/"otterize-validating-webhook-configuration.yaml
