6ec032abbda0571cf99c0439070cd763ac586b67
Ori Shoshan <ori@otterize.com>
2023-11-18 16:25:30 +0100

    Fix bad webhook config and make GenerateRoleName & GenerateRoleARN public

--- src/shared/awsagent/policies.go
+++ src/shared/awsagent/policies.go
@@ -21,8 +21,7 @@ func (a *Agent) AddRolePolicy(ctx context.Context, namespace, accountName, polic
 	}
 
 	if !exists {
-		// TODO: is this ok? perhaps we need to retry when the pod starts again with a pod
-		return fmt.Errorf("role not found: %s", generateRoleName(a.clusterName, namespace, accountName))
+		return fmt.Errorf("role not found: %s", a.GenerateRoleName(namespace, accountName))
 	}
 
 	policyArn := a.generatePolicyArn(namespace, policyName)
@@ -123,7 +122,7 @@ func (a *Agent) DeleteRolePolicy(ctx context.Context, namespace, policyName stri
 
 func (a *Agent) SetRolePolicy(ctx context.Context, namespace, accountName string, statements []StatementEntry) error {
 	logger := logrus.WithField("account", accountName).WithField("namespace", namespace)
-	roleName := generateRoleName(a.clusterName, namespace, accountName)
+	roleName := a.GenerateRoleName(namespace, accountName)
 
 	exists, role, err := a.GetOtterizeRole(ctx, namespace, accountName)
 
