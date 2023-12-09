a31217c1d92f1514060d0a3a2e0603b6638c5f01
aerosouund <aerosound161@gmail.com>
2023-12-09 14:13:15 +0200

    linkerd labels

--- src/operator/api/v1alpha3/clientintents_types.go
+++ src/operator/api/v1alpha3/clientintents_types.go
@@ -52,6 +52,8 @@ const (
 	OtterizeServiceNetworkPolicyNameTemplate             = "svc-access-to-%s-from-%s"
 	OtterizeNetworkPolicy                                = "intents.otterize.com/network-policy"
 	OtterizeSvcNetworkPolicy                             = "intents.otterize.com/svc-network-policy"
+	OtterizeLinkerdMeshTLSAnnotationKey                  = "intents.otterize.com/linkerd-authenticates"
+	OtterizeLinkerdServerAnnotationKey                   = "intents.otterize.com/linkerd-server"
 	OtterizeNetworkPolicyServiceDefaultDeny              = "intents.otterize.com/network-policy-service-default-deny"
 	OtterizeNetworkPolicyExternalTraffic                 = "intents.otterize.com/network-policy-external-traffic"
 	ClientIntentsFinalizerName                           = "intents.otterize.com/client-intents-finalizer"
