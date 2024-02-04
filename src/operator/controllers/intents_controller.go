f0d3962adb89a4d8d59a8374d7c2653d98958c7e
omri.s <omri.s@otterize.com>
2024-02-04 19:34:07 +0200

    fixup

--- src/operator/controllers/intents_controller.go
+++ src/operator/controllers/intents_controller.go
@@ -28,6 +28,7 @@ import (
 	"github.com/otterize/intents-operator/src/shared/operator_cloud_client"
 	"github.com/otterize/intents-operator/src/shared/reconcilergroup"
 	"github.com/otterize/intents-operator/src/shared/serviceidresolver"
+	"github.com/otterize/intents-operator/src/shared/serviceidresolver/serviceidentity"
 	"github.com/otterize/intents-operator/src/shared/telemetries/telemetriesconfig"
 	"github.com/samber/lo"
 	"github.com/sirupsen/logrus"
@@ -299,13 +300,8 @@ func (r *IntentsReconciler) InitIntentsServerIndices(mgr ctrl.Manager) error {
 					res = append(res, otterizev1alpha3.OtterizeInternetTargetName)
 					continue
 				}
-				serverName := intent.GetTargetServerName()
-				if intent.IsTargetServerKubernetesService() {
-					serverName = "svc." + serverName
-				}
-				serverNamespace := intent.GetTargetServerNamespace(intents.Namespace)
-				formattedServerName := otterizev1alpha3.GetFormattedOtterizeIdentity(serverName, serverNamespace)
-				res = append(res, formattedServerName)
+				service := serviceidentity.NewFromIntent(intent, intents.Namespace)
+				res = append(res, service.GetFormattedOtterizeIdentity())
 			}
 
 			return res
