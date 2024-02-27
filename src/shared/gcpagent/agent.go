5d0624dfc4242d0f75b94ab8a75b86dac73f9085
davidrobert <david.r@otterize.com>
2024-02-27 20:32:37 +0200

    pr fixes

--- src/shared/gcpagent/agent.go
+++ src/shared/gcpagent/agent.go
@@ -1,7 +1,9 @@
 package gcpagent
 
 import (
+	"cloud.google.com/go/compute/metadata"
 	"context"
+	"github.com/otterize/intents-operator/src/shared/errors"
 	"github.com/sirupsen/logrus"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -16,18 +18,18 @@ func NewGCPAgent(ctx context.Context, c client.Client) (*Agent, error) {
 	logrus.Info("Initializing GCP Intents agent")
 
 	// Get the current GCP project using the metadata server
-	projectID := "otterize-gcp-integration"
-	//projectID, err := metadata.ProjectID()
-	//if err != nil {
-	//	return nil, errors.Errorf("failed to get current GCP project: %w", err)
-	//}
+	//projectID := "otterize-gcp-integration" // TODO: cleanup
+	projectID, err := metadata.ProjectID()
+	if err != nil {
+		return nil, errors.Errorf("failed to get current GCP project: %w", err)
+	}
 
 	// Retrieve the cluster name using the metadata server
-	clusterName := "otterize-iam-gke-tutorial"
-	//clusterName, err := metadata.InstanceAttributeValue("cluster-name")
-	//if err != nil {
-	//	return nil, errors.Errorf("failed to get current GKE cluster: %w", err)
-	//}
+	//clusterName := "otterize-iam-gke-tutorial" // TODO: cleanup
+	clusterName, err := metadata.InstanceAttributeValue("cluster-name")
+	if err != nil {
+		return nil, errors.Errorf("failed to get current GKE cluster: %w", err)
+	}
 
 	return &Agent{
 		client:      c,
