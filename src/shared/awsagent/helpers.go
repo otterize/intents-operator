9f9227ff92e56f4bc666425106eff8efc5bc03d9
omris94 <46892443+omris94@users.noreply.github.com>
2024-03-03 16:42:27 +0200

    Add support for soft deletion of AWS roles and policies. It is done by tagging them as soft-deleted instead of actually deleting them (#361)

---
+++ src/shared/awsagent/helpers.go
@@ -0,0 +1,18 @@
+package awsagent
+
+import (
+	"github.com/aws/aws-sdk-go-v2/service/iam/types"
+	"github.com/samber/lo"
+)
+
+func HasSoftDeleteStrategyTagSet(tags []types.Tag) bool {
+	return lo.SomeBy(tags, func(tag types.Tag) bool {
+		return lo.FromPtr(tag.Key) == softDeletionStrategyTagKey && lo.FromPtr(tag.Value) == softDeletionStrategyTagValue
+	})
+}
+
+func hasSoftDeletedTagSet(tags []types.Tag) bool {
+	return lo.SomeBy(tags, func(tag types.Tag) bool {
+		return lo.FromPtr(tag.Key) == softDeletedTagKey
+	})
+}
