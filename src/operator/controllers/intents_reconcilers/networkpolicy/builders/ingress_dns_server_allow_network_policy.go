dfd905814a4cb82bbcc1f04b28731ad032876901
Ori Shoshan <ori@otterize.com>
2024-03-12 17:53:55 +0100

    Add test

--- src/operator/controllers/intents_reconcilers/networkpolicy/builders/ingress_dns_server_allow_network_policy.go
+++ src/operator/controllers/intents_reconcilers/networkpolicy/builders/ingress_dns_server_allow_network_policy.go
@@ -2,7 +2,6 @@ package builders
 
 import (
 	"context"
-	"github.com/amit7itz/goset"
 	"github.com/otterize/intents-operator/src/operator/effectivepolicy"
 	"github.com/otterize/intents-operator/src/shared/injectablerecorder"
 	"github.com/samber/lo"
@@ -23,31 +22,16 @@ func NewIngressDNSServerAutoAllowNetpolBuilder() *IngressDNSServerAutoAllowNetpo
 // Add UDP allow port 53 if the target is a DNS server in kube-system. This covers 'coredns' and 'kube-dns'.
 func (r *IngressDNSServerAutoAllowNetpolBuilder) buildIngressRulesFromServiceEffectivePolicy(ep effectivepolicy.ServiceEffectivePolicy) []v1.NetworkPolicyIngressRule {
 	ingressRules := make([]v1.NetworkPolicyIngressRule, 0)
-	fromNamespaces := goset.NewSet[string]()
-	for _, call := range ep.CalledBy {
-		if call.IntendedCall.IsTargetOutOfCluster() {
-			continue
-		}
-		if call.IntendedCall.IsTargetServerKubernetesService() {
-			continue
-		}
-		// We use the same from.podSelector for every namespace,
-		// therefore there is no need for more than one ingress rule per namespace
-		if fromNamespaces.Contains(call.Service.Namespace) {
-			continue
-		}
 
-		if !strings.HasSuffix(call.IntendedCall.Name, "dns") || call.IntendedCall.GetTargetServerNamespace(call.Service.Namespace) != "kube-system" {
-			continue
-		}
-		fromNamespaces.Add(call.Service.Namespace)
-		ingressRules = append(ingressRules, v1.NetworkPolicyIngressRule{
-			Ports: []v1.NetworkPolicyPort{{
-				Protocol: lo.ToPtr(corev1.ProtocolUDP),
-				Port:     lo.ToPtr(intstr.FromInt32(53)),
-			}},
-		})
+	if !strings.HasSuffix(ep.Service.Name, "dns") || ep.Service.Namespace != "kube-system" {
+		return ingressRules
 	}
+	ingressRules = append(ingressRules, v1.NetworkPolicyIngressRule{
+		Ports: []v1.NetworkPolicyPort{{
+			Protocol: lo.ToPtr(corev1.ProtocolUDP),
+			Port:     lo.ToPtr(intstr.FromInt32(53)),
+		}},
+	})
 	return ingressRules
 }
 
