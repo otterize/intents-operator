1627b8ecf4fa04b8fb651765a846b6dd10a9c0ec
omri.s <omri.s@otterize.com>
2024-03-25 20:55:00 +0100

    fixup

--- src/operator/controllers/intents_reconcilers/networkpolicy/builders/port_egress_network_policy.go
+++ src/operator/controllers/intents_reconcilers/networkpolicy/builders/port_egress_network_policy.go
@@ -77,20 +77,14 @@ func getEgressRuleBasedOnServicePodSelector(svc *corev1.Service) v1.NetworkPolic
 		},
 	}
 
-	portToProtocol := make(map[intstr.IntOrString]corev1.Protocol)
-	// Gather all target ports (target ports in the pod the service proxies to)
-	for _, port := range svc.Spec.Ports {
-		portToProtocol[port.TargetPort] = port.Protocol
-	}
-
-	networkPolicyPorts := make([]v1.NetworkPolicyPort, 0)
 	// Create a list of network policy ports
-	for port, protocol := range portToProtocol {
+	networkPolicyPorts := make([]v1.NetworkPolicyPort, 0)
+	for _, port := range svc.Spec.Ports {
 		netpolPort := v1.NetworkPolicyPort{
-			Port: lo.ToPtr(port),
+			Port: lo.ToPtr(port.TargetPort),
 		}
-		if len(protocol) != 0 {
-			netpolPort.Protocol = lo.ToPtr(protocol)
+		if len(port.Protocol) != 0 {
+			netpolPort.Protocol = lo.ToPtr(port.Protocol)
 		}
 		networkPolicyPorts = append(networkPolicyPorts, netpolPort)
 	}
