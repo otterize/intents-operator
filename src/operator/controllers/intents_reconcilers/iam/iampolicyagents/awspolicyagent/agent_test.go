02187b4e9f9acc4a870812ebcd98474044bc5554
Amit Lichtenberg <amitlicht@otterize.com>
2024-04-08 20:40:07 +0300

    Add UT for createPolicyFromIntents with templating

--- src/operator/controllers/intents_reconcilers/iam/iampolicyagents/awspolicyagent/agent_test.go
+++ src/operator/controllers/intents_reconcilers/iam/iampolicyagents/awspolicyagent/agent_test.go
@@ -1,6 +1,7 @@
 package awspolicyagent
 
 import (
+	otterizev1alpha3 "github.com/otterize/intents-operator/src/operator/api/v1alpha3"
 	"github.com/otterize/intents-operator/src/shared/awsagent"
 	"github.com/stretchr/testify/suite"
 	"testing"
@@ -23,6 +24,27 @@ func (s *AWSAgentPolicySuite) Test_templateResourceName() {
 	resourceName := agent.templateResourceName("arn:aws:sqs:$(AWS_REGION):$(AWS_ACCOUNT_ID):queue1")
 	// Then
 	s.Equal("arn:aws:sqs:test-region:test-accountid:queue1", resourceName)
+}
+
+func (a *AWSAgentPolicySuite) Test_createPolicyFromIntents_TemplateResourceName() {
+	// Given
+	agent := &Agent{
+		Agent: &awsagent.Agent{
+			Region:    "test-region",
+			AccountID: "test-accountid",
+		},
+	}
+	intents := []otterizev1alpha3.Intent{
+		{
+			Name:       "arn:aws:sqs:$(AWS_REGION):$(AWS_ACCOUNT_ID):queue1",
+			AWSActions: []string{"sqs:SendMessage"},
+		},
+	}
+	// When
+	policyDoc := agent.createPolicyFromIntents(intents)
+	// Then
+	a.Equal("arn:aws:sqs:test-region:test-accountid:queue1", policyDoc.Statement[0].Resource)
+
 }
 
 func TestRunAWSAgentPolicySuite(t *testing.T) {
