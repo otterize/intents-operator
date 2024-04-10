a4b85a7586f68e6528dba254bb7add75bdc0032c
Ori Shoshan <ori@otterize.com>
2024-04-10 19:48:59 +0300

    Experimental feature: use Docker image name to as workload identifier when randomized Jobs with no parent would be the workload identifier

--- src/shared/serviceidresolver/config.go
+++ src/shared/serviceidresolver/config.go
@@ -8,11 +8,13 @@ import (
 const (
 	serviceNameOverrideAnnotationKey        = "service-name-override-annotation"
 	serviceNameOverrideAnnotationKeyDefault = "intents.otterize.com/service-name"
+	useImageNameForServiceIDForJobs         = "use-image-name-for-service-id-for-jobs"
 	EnvPrefix                               = "OTTERIZE"
 )
 
 func init() {
 	viper.SetDefault(serviceNameOverrideAnnotationKey, serviceNameOverrideAnnotationKeyDefault)
+	viper.SetDefault(useImageNameForServiceIDForJobs, false)
 	viper.SetEnvPrefix(EnvPrefix)
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	viper.AutomaticEnv()
