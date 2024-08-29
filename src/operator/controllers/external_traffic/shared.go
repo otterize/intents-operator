5a7f7a3af17a3a67000813987629ec01affc72ad
Ori Shoshan <ori@otterize.com>
2024-08-29 20:57:41 +0300

    Update src/operator/controllers/external_traffic/shared.go
    Co-authored-by: omris94 <46892443+omris94@users.noreply.github.com>

--- src/operator/controllers/external_traffic/shared.go
+++ src/operator/controllers/external_traffic/shared.go
@@ -31,7 +31,7 @@ func isIngressListHasIPAWSALB(ingressList []v1.Ingress) bool {
 			return false
 		}
 
-		scheme, ok := ingress.Annotations["alb.ingress.kubernetes.io/target-type"]
+		targetType, ok := ingress.Annotations["alb.ingress.kubernetes.io/target-type"]
 		if !ok {
 			return false
 		}
