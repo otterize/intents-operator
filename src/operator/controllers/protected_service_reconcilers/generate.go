6b60f783ea5b5e56b36427b861638ef0296de8bf
omri.s <omri.s@otterize.com>
2024-12-18 16:38:37 +0200

    fixup

--- src/operator/controllers/protected_service_reconcilers/generate.go
+++
@@ -1,3 +0,0 @@
-package protected_service_reconcilers
-
-//go:generate go run go.uber.org/mock/mockgen@v0.2.0 -package protectedservicesmock -destination ./mocks/ext_netpol_handler_mock.go -source ./default_deny.go ExternalNepolHandler
