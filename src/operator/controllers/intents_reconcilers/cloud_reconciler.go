ea58f7b37935e2f167ba0ac43bfc4f1cf45ae57a
omri.s <omri.s@otterize.com>
2025-01-16 19:51:40 +0200

    Support reporting of `kind: Service` applied intents to the cloud

--- src/operator/controllers/intents_reconcilers/cloud_reconciler.go
+++ src/operator/controllers/intents_reconcilers/cloud_reconciler.go
@@ -11,11 +11,7 @@ import (
 	"github.com/samber/lo"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
-	corev1 "k8s.io/api/core/v1"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -54,12 +50,7 @@ func (r *OtterizeCloudReconciler) Reconcile(ctx context.Context, req reconcile.R
 		return intents.DeletionTimestamp == nil
 	})
 
-	clientIntentsListConverted, err := r.convertK8sServicesToOtterizeIdentities(ctx, clientIntentsList)
-	if err != nil {
-		return ctrl.Result{}, errors.Wrap(err)
-	}
-
-	intentsInput, err := clientIntentsListConverted.FormatAsOtterizeIntents(ctx, r.Client)
+	intentsInput, err := clientIntentsList.FormatAsOtterizeIntents(ctx, r.Client)
 	if err != nil {
 		return ctrl.Result{}, errors.Wrap(err)
 	}
@@ -75,68 +66,6 @@ func (r *OtterizeCloudReconciler) Reconcile(ctx context.Context, req reconcile.R
 	logrus.Debugf("successfully reported %d applied intents", len(clientIntentsList.Items))
 
 	return ctrl.Result{}, nil
-}
-
-func (r *OtterizeCloudReconciler) convertK8sServicesToOtterizeIdentities(
-	ctx context.Context,
-	clientIntentsList *otterizev2alpha1.ClientIntentsList) (*otterizev2alpha1.ClientIntentsList, error) {
-
-	// TODO: Remove when access graph supports Kubernetes services
-	for _, clientIntent := range clientIntentsList.Items {
-		callList := make([]otterizev2alpha1.Target, 0)
-		for _, intent := range clientIntent.GetTargetList() {
-			if !intent.IsTargetServerKubernetesService() {
-				callList = append(callList, intent)
-				continue
-			}
-			if intent.IsTargetTheKubernetesAPIServer(clientIntent.Namespace) {
-				intentCopy := intent.DeepCopy()
-				if intent.Kubernetes != nil {
-					intentCopy.Kubernetes.Name = intent.GetServerFullyQualifiedName(clientIntent.Namespace)
-				}
-				if intent.Service != nil {
-					intentCopy.Service.Name = intent.GetServerFullyQualifiedName(clientIntent.Namespace)
-				}
-				callList = append(callList, intent)
-				continue
-			}
-
-			svc := corev1.Service{}
-			kubernetesSvcName := intent.GetTargetServerName()
-			kubernetesSvcNamespace := intent.GetTargetServerNamespace(clientIntent.Namespace)
-			err := r.Get(ctx, types.NamespacedName{
-				Namespace: kubernetesSvcNamespace,
-				Name:      kubernetesSvcName,
-			}, &svc)
-			if err != nil {
-				if k8serrors.IsNotFound(err) {
-					continue
-				}
-				return nil, errors.Wrap(err)
-			}
-			podList := corev1.PodList{}
-			err = r.List(ctx, &podList, &client.ListOptions{LabelSelector: labels.SelectorFromSet(svc.Spec.Selector)})
-			if err != nil {
-				return nil, errors.Wrap(err)
-			}
-			if len(podList.Items) != 0 {
-				otterizeIdentity, err := r.serviceIdResolver.ResolvePodToServiceIdentity(ctx, &podList.Items[0])
-				if err != nil {
-					return nil, errors.Wrap(err)
-				}
-				if intent.Kubernetes != nil {
-					intent.Kubernetes.Name = otterizeIdentity.Name
-				}
-				if intent.Service != nil {
-					intent.Service.Name = otterizeIdentity.Name
-				}
-				callList = append(callList, intent)
-			}
-			clientIntent.Spec.Targets = callList
-		}
-	}
-
-	return clientIntentsList, nil
 }
 
 func (r *OtterizeCloudReconciler) SetupWithManager(mgr ctrl.Manager) error {
