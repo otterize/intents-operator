74a7dbd5e81fc487d4fa7941cb94e603b47fefe4
omri.s <omri.s@otterize.com>
2025-03-17 19:23:32 +0200

    fixup

--- src/shared/otterizecloud/graphqlclient/generate.go
+++ src/shared/otterizecloud/graphqlclient/generate.go
@@ -2,5 +2,5 @@ package graphqlclient
 
 import _ "github.com/suessflorian/gqlfetch"
 
-//go:generate sh -c "go run github.com/suessflorian/gqlfetch/gqlfetch --endpoint https://app-sapirwo-cloud-pr2974.staging.otterize.com/api/graphql/v1beta > schema.graphql"
+//go:generate sh -c "go run github.com/suessflorian/gqlfetch/gqlfetch --endpoint https://app.staging.otterize.com/api/graphql/v1beta > schema.graphql"
 //go:generate go run github.com/Khan/genqlient ./genqlient.yaml
